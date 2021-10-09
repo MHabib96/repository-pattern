27eb88d24386575122ab10af74919490fa91ae97
MHabib96 <33306932+MHabib96@users.noreply.github.com>
2021-10-10 00:52:03 +0100

    Add database context

---
+++ Database/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPattern.Models;
+
+namespace RepositoryPattern.Database
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
