c0cf297c8a4215e2e8d6142c4556e89650443e32
MHabib96 <33306932+MHabib96@users.noreply.github.com>
2021-10-10 18:13:09 +0100

    Add developer controller
     - controller to demo the use of Unit of Work pattern

---
+++ Controllers/DeveloperController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPattern.Models;
+using RepositoryPattern.UnitOfWork.Interfaces;
+
+namespace RepositoryPattern.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class DeveloperController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeveloperController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public Developer Get(Guid id)
+        {
+            return _unitOfWork.Developers.GetById(id);
+        }
+
+        [HttpGet]
+        public IEnumerable<Developer> GetAll()
+        {
+            return _unitOfWork.Developers.GetAll();
+        }
+    }
+}
